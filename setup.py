c8b53fe4b47c43d63c9e4ba9a288b8bb23aa33ae
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2017-02-28 12:34:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='lazy-consul',
-    version='0.1',
+    version='0.1.1',
     description='Lazy client library for the Consul Key/Value store',
     platforms="all",
     classifiers=[
-        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Clustering',
     ],
+    license='MIT',
     author='Alexander Tikhonov',
+    url='https://github.com/Fatal1ty/lazy-consul',
     packages=find_packages(exclude=('tests',)),
     install_requires=[
         'python-consul'
